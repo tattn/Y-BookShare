992bf5377994a030cb824c48e060ea6ced50400a
tarte-0g <tacktogla@gmail.com>
2015-07-08 18:12:23 +0900

    modify genre_id

--- spec/support/factories/books.rb
+++ spec/support/factories/books.rb
@@ -2,7 +2,7 @@
 FactoryGirl.define do
   factory :book do
 	sequence(:title) {|n| "Book#{n}"}
-	genre {Genre.all.to_a.map(&:name).sample}	
+	genre_id {Genre.all.to_a.map(&:id).sample}	
   end
 end
 
