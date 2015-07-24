0e0404c7932d0bee9aeb2368dae46ff7420db867
tarte-0g <tacktogla@gmail.com>
2015-07-24 21:11:15 +0900

    modify blacklist

--- app/api/v1/users.rb
+++ app/api/v1/users.rb
@@ -147,39 +147,6 @@ module V1
           end
         end
 
-        resource :blacklist do
-          desc "get a blacklist"
-          get '/', jbuilder: 'users/users' do
-            authenticate!
-            black_ids = Blacklist.where(user_id: @current_user.user_id).map(&:bother_id)
-            @users = User.where(user_id: black_ids)
-          end
-
-          desc "add blacklist"
-          params do
-            requires :user_id, type: Integer, desc: "block user id"
-          end
-          post '/', jbuilder: 'empty' do
-            authenticate!
-            emit_error! "既に登録されています", 400, 1 if Blacklist.find_by user_id: @current_user.user_id bother_id: params[:user_id]
-            Blacklist.create user_id: @current_user.user_id bother_id: params[:user_id]
-          end
-
-          params do
-            requires :user_id, type: Integer, desc: "user id"
-          end
-          route_param :user_id do
-            desc "acquit a user"
-            delete '/', jbuilder: 'empty' do
-              authenticate!
-              blacklist = Blacklist.find_by user_id: @current_user.user_id bother_id: params[:user_id]
-              emit_error! "ブラックリストに登録されていません", 400, 1 unless blacklist
-              blacklist.destroy
-            end
-          end
-          
-        end
-
         resource :borrow do
           get '/', jbuilder: 'borrows/borrows' do
             authenticate!
