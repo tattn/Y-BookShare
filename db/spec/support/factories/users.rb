37ce928c3524bcf06f2e06ade2f177442dfa9c14
Tatsuya Tanaka <tatsuyars@yahoo.co.jp>
2015-07-31 11:39:05 +0900

    Add sample comments to seed of user

--- db/spec/support/factories/users.rb
+++ db/spec/support/factories/users.rb
@@ -4,9 +4,10 @@ FactoryGirl.define do
 
   firstname = ["上総","達也","英礼","拓人","侑弥"]
   lastname = ["坂本","田中","山本","小倉","原田"]
+	comment = ["進撃の巨人読みたい", "解体新書面白い", "眠い"]
 
   factory :user do
-  	sequence(:user_id) {|n| n}	
+  	sequence(:user_id) {|n| n}
 	sequence(:email) {|n| "email#{n}@***.com"}
 	firstname {firstname.pop}
 	lastname {lastname.pop}
@@ -14,6 +15,7 @@ FactoryGirl.define do
 	sequence(:borrow_num) {|n| if n==2 then 1 else 0 end}
 	school {"青山学院大学"}
 	password {'1234'}
+	comment { comment.sample }
 	sequence(:invitation_code) {|n| SecureRandom.hex }
   end
 end
