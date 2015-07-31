587e3cb6469f9403eafac58ec7a13916ec1796cb
tanakasan2525 <tatsuyars@yahoo.co.jp>
2015-07-31 21:23:23 +0900

    bugfix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	mount API => '/bookshare/'
 
-  get 'admindebug' => 'admindebug#index'
+  get '/bookshare/admindebug' => 'admindebug#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
