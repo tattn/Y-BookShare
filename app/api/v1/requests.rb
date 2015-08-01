860bb8b5d7b7d522fc499c241f84156d839f3e13
Tatsuya Tanaka <tatsuyars@yahoo.co.jp>
2015-08-02 05:06:21 +0900

    Add app/api/v1/requests.rb

---
+++ app/api/v1/requests.rb
@@ -0,0 +1,20 @@
+module V1
+  class Requests < Grape::API
+    # このクラス内で共通化出来る処理は helper に書く
+    helpers do
+      include V1::Helpers    # emit_empty などを使えるようにする（必須）
+    end
+
+    params do
+      requires :token, type: String, desc: "Access token"
+    end
+    resource :my do
+      resource :request do
+        get '/sent', jbuilder: 'requests/requests' do
+          authenticate!
+          @reqs = Request.where(sender_id: @current_user.user_id)
+        end
+      end
+    end
+  end
+end
